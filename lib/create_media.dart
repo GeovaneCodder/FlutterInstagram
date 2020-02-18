ff0f3b07e910359b4ae178eb7cb8c285bbf4a1cb
Geovane Souza <geovanecodder@gmail.com>
2020-02-18 14:10:43 -0300

    Finish screen add media

--- lib/create_media.dart
+++ lib/create_media.dart
@@ -24,8 +24,14 @@ class _CreateMediaState extends State<CreateMedia> {
     super.dispose();
   }
 
-  Future<void> _getPermission() async =>
+  Future<void> _getPermission() async {
+    PermissionStatus permission = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.contacts);
+
+    if (permission != PermissionStatus.granted) {
       await PermissionHandler().requestPermissions([PermissionGroup.camera]);
+    }
+  }
 
   Future<void> _initCamera() async {
     _cameras = await availableCameras();
@@ -75,31 +81,179 @@ class _CreateMediaState extends State<CreateMedia> {
       return Container();
     }
 
-    return Stack(
-      children: <Widget>[
-        _buildCameraPreview(),
-        SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(left: 20, right: 20, top: 30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: Icon(
-                    Icons.arrow_back,
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _buildCameraPreview(),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.only(left: 20, right: 20, top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Icon(
+                    Icons.settings,
                     color: Colors.white,
                   ),
-                ),
-                Icon(
-                  Icons.settings,
-                  color: Colors.white,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Stack(children: <Widget>[
+              ClipPath(
+                clipper: BottomBarClipper(),
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 4,
+                  color: Colors.black.withOpacity(.3),
+                  child: Column(
+                    children: <Widget>[
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 40),
+                          child: Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(width: 2, color: Colors.white),
+                            ),
+                            child: Center(
+                              child: Container(
+                                width: 20,
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        height: 50,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            Text(
+                              'Juno',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Text(
+                              'Juno',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Text(
+                              'Juno',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Text(
+                              'Juno',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Text(
+                              'Juno',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Text(
+                              'Juno',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Text(
+                              'Juno',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 20,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.flash_off,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.refresh,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
     );
   }
+}
+
+class BottomBarClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    var sw = size.width;
+    var sh = size.height;
+
+    path.moveTo(0, sh * 0.40);
+    path.lineTo(sw * 0.1, sh * 0.40);
+    path.cubicTo(sw * 0.3, sh * 0.40, sw * 0.4, 0, sw * 0.5, 0);
+    path.cubicTo(sw * 0.6, 0, sw * 0.7, sh * 0.40, sw * 0.9, sh * 0.40);
+    path.lineTo(sw, sh * 0.40);
+    path.lineTo(sw, sh);
+    path.lineTo(0, sh);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
